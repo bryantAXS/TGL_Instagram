1f89d4a96d25092224968bd35409588166839d4c
Bryant Hughes <bryant@thegoodlab.com>
2012-01-23 22:16:05 -0600

    more updates, nearly done building module, last step is adding page url to cache

--- system/expressionengine/third_party/tgl_instagram/mod.tgl_instagram.php
+++ system/expressionengine/third_party/tgl_instagram/mod.tgl_instagram.php
@@ -5,19 +5,18 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 require_once PATH_THIRD . 'tgl_instagram/classes/Instagram.php';
 
 /**
- * The Antenna plugin will generate the YouTube or Vimeo embed
- * code for a single YouTube or Vimeo clip. It will also give
- * you back the Author, their URL, the video title,
- * and a thumbnail.
+ * TGL Instagram is a EE Module allowing a user to authenticate with the Instagram API, and to retrieve infromation from it using embedded template tags.
+ * 
+ *  For more information please see the repository here:  
  *
- * @package Antenna
+ * @package Tgl
  */
 
 class Tgl_instagram 
 {
 	public $return_data = '';
 	public $cache_name = 'tgl_instagram';
-	public $refresh_cache = 120;			// in mintues
+	public $refresh_cache = 60;			// in mintues
 	public $cache_expired = FALSE;
 
 	public function Tgl_instagram() 
@@ -37,42 +36,59 @@ class Tgl_instagram
 		
 	}
 
-	function user_feed(){
+	/**
+	 * Spits out array nicely
+	 * @param  array $data
+	 * @return void
+	 */
+	function _dump($data){
+		echo "<pre>";
+		echo print_r($data);
+		echo "</pre>";
+	}
+
+	/**
+	 * Returns the authenticated user's feed.
+	 * @return [type]
+	 */
+	function feed(){
 		
-		$user_name = $this->EE->TMPL->fetch_param('username');
-		$limit = $this->EE->TMPL->fetch_param('limit');
+		//params from the template tag
+		$params = array();
+		$params['limit'] = $this->EE->TMPL->fetch_param('limit', null);
 
-		$user_id = $this->_get_user_id($user_name);
+		//check to see if we have cached data to use
+		if($cached_data = $this->_check_cache($params)){
+			return $cached_data;
+		}
 
-		$user_feed_data = $this->instagram->getUserRecent($user_id);
-		$response = json_decode($user_feed_data, true);
+		//no cache data to use, lets hit the api
+		$feed_data = $this->instagram->getUserFeed(null,null,$params['limit']);
 
+		//extract
+		$response = json_decode($feed_data, true);
+
+		//if there is no data, return
+		if( ! isset($response['data']) && count($response['data']) < 1)
+		{
+			return FALSE;
+		}
+
+		//get data between ee tags
 		$tagdata = $this->EE->TMPL->tagdata;
 
-		//$this->_dump($response);
-
+		//variables to be used when we parse the tagdata
 		$variables = array();
 		$count = 0;
 
 		foreach ($response['data'] as $data) {
 		  
-		  if($count == $limit) break;
+		  if($params['limit'] != null && $count == $params['limit']) break;
 
 		  $row_variables = array();
-		  $row_variables['tags'] = $data['tags'];
-		  $row_variables['filter'] = $data['filter'];
-		  $row_variables['created_at'] = $data['created_time'];
-		  $row_variables['link'] = $data['link'];
-
-		  $row_variables['thumbnail_url'] = $data['images']['thumbnail']['url'];
-		  $row_variables['thumbnail'] = "<img src='".$data['images']['thumbnail']['url']."' width='".$data['images']['thumbnail']['width']."' height='".$data['images']['thumbnail']['height']."'/>";
-
-		  $row_variables['low_url'] = $data['images']['thumbnail']['url'];
-		  $row_variables['low'] = "<img src='".$data['images']['thumbnail']['url']."' width='".$data['images']['thumbnail']['width']."' height='".$data['images']['thumbnail']['height']."'/>";
-
-		  $row_variables['standard_url'] = $data['images']['thumbnail']['url'];
-		  $row_variables['standard'] = "<img src='".$data['images']['thumbnail']['url']."' width='".$data['images']['thumbnail']['width']."' height='".$data['images']['thumbnail']['height']."'/>";
-
+		  
+		  $row_variables = $this->_parse_row_data($data, $row_variables);
+		  
 		  $variables[] = $row_variables;
     	
     	$count++;
@@ -80,18 +96,184 @@ class Tgl_instagram
   	}
 
     $return_data = $this->EE->TMPL->parse_variables($tagdata, $variables);
-    
+
+    $this->_write_cache($return_data, $params);
+
     return $return_data;
 
 	}
 
-	function _dump($data){
-		echo "<pre>";
-		echo print_r($data);
-		echo "</pre>";
+
+	/**
+	 * Returns a specific user's own feed (only pictures they explicitly took).
+	 * @return [type]
+	 */
+	function user_feed(){
+		
+		$params = array();
+		$params['user_name'] = $this->EE->TMPL->fetch_param('username');
+		$params['limit'] = $this->EE->TMPL->fetch_param('limit');
+
+		//make sure we have the required parameters
+		if(! isset($params['user_name']))
+		{
+			return FALSE;
+		}
+
+		if($cached_data = $this->_check_cache($params)){
+			return $cached_data;
+		}
+
+		//get the user id for the api call
+		$user_id = $this->_get_user_id($params['user_name']);
+		$user_feed_data = $this->instagram->getUserRecent($user_id);
+		$response = json_decode($user_feed_data, true);
+		
+		//if there is no data, return
+		if( ! isset($response['data']) && count($response['data']) < 1)
+		{
+			return FALSE;
+		}	
+
+		$tagdata = $this->EE->TMPL->tagdata;
+
+		$variables = array();
+		$count = 0;
+
+		foreach ($response['data'] as $data) {
+		  
+		  if($params['limit'] != null && $count == $params['limit']) break;
+
+		  $row_variables = array();
+		  
+		  $row_variables = $this->_parse_row_data($data, $row_variables);
+		  
+		  $variables[] = $row_variables;
+    	
+    	$count++;
+
+  	}
+
+    $return_data = $this->EE->TMPL->parse_variables($tagdata, $variables);
+
+    $this->_write_cache($return_data, $params);
+
+    return $return_data;
+
 	}
 
-	function _get_user_id($username){
+	/**
+	 * Extracts data from a single row returned from the API and assigns it to our row_variables array which eventually gets used to parse our template
+	 * @param  array $data          a single row's data from the Instagram API
+	 * @param  array $row_variables An array of data for a row will be parsing, something this is blank and sometimes it might hold other data that has already been parsed.
+	 * @return array
+	 */
+	function _parse_row_data($data, $row_variables = null)
+	{
+		
+		if(! $row_variables) $row_variables = array();
+
+	  //static data
+	  //$row_variables['tags'] = $data['tags'];
+	  $row_variables['filter'] = $data['filter'];
+	  $row_variables['created_at'] = $data['created_time'];
+	  $row_variables['link'] = $data['link'];
+
+	  //caption
+	  if(count($data['caption']) > 0){
+	  	$row_variables['caption'] = $data['caption']['text'];
+	  }
+
+	  //comments
+	  if(count($data['comments']) > 0){
+	  	
+	  	$row_variables['comment_count'] = $data['comments']['count'];
+	  	$row_variables['comments'] = $this->_parse_comments($data['comments']['data']);
+	  }
+
+	  //user
+	  $row_variables['username'] = $data['user']['username'];
+	  $row_variables['website'] = $data['user']['website'];
+	  $row_variables['bio'] = $data['user']['bio'];
+	  $row_variables['profile_picture'] = $data['user']['profile_picture'];
+	  $row_variables['full_name'] = $data['user']['full_name'];
+
+	  //images
+	  $row_variables['thumbnail_url'] = $data['images']['thumbnail']['url'];
+	  $row_variables['thumbnail'] = "<img src='".$data['images']['thumbnail']['url']."' width='".$data['images']['thumbnail']['width']."' height='".$data['images']['thumbnail']['height']."'/>";
+
+	  //likes
+	  if(count($data['likes']) > 0){
+	  	
+	  	$row_variables['likes_count'] = $data['likes']['count'];
+	  	$row_variables['likes'] = $this->_parse_likes($data['likes']['data']);
+	  }
+
+	  $row_variables['low_url'] = $data['images']['low_resolution']['url'];
+	  $row_variables['low'] = "<img src='".$data['images']['low_resolution']['url']."' width='".$data['images']['low_resolution']['width']."' height='".$data['images']['low_resolution']['height']."'/>";
+
+	  $row_variables['standard_url'] = $data['images']['standard_resolution']['url'];
+	  $row_variables['standard'] = "<img src='".$data['images']['standard_resolution']['url']."' width='".$data['images']['standard_resolution']['width']."' height='".$data['images']['standard_resolution']['height']."'/>";
+
+	  return $row_variables;
+
+	}
+
+	/**
+	 * set comment returned from the api
+	 * @param  array $data comment data from api
+	 * @return array
+	 */
+	function _parse_comments($data)
+	{
+		
+		$comments = array();
+		
+		foreach ($data as $row)
+		{
+			$comment = array();
+			$comment['comment_text'] = $row['text']; 
+			$comment['comment_username'] = $row['from']['username']; 
+			$comment['comment_profile_picture'] = $row['from']['profile_picture']; 
+			$comment['comment_full_name'] = $row['from']['full_name'];
+			
+			$comments[] = $comment; 
+		}
+
+		return $comments;
+
+	}
+
+	/**
+	 * set likes data from the api
+	 * @param  array $data likes data from the api
+	 * @return array
+	 */
+	function _parse_likes($data)
+	{
+
+		$likes = array();
+		
+		foreach ($data as $row)
+		{
+			$like = array();
+			$like['like_username'] = $row['username']; 
+			$like['like_full_name'] = $row['full_name']; 
+			$like['like_profile_picture'] = $row['profile_picture']; 
+			$likes[] = $like; 
+		}
+
+		return $likes;
+
+	}
+
+	/**
+	 * Given the user's user name, it will return the user id
+	 * @param  string $username user's instagram hand;e
+	 * @return string/int
+	 */
+	function _get_user_id($username)
+	{
 		
 		$user_data = $this->instagram->searchUser($username);
 		$response = json_decode($user_data, true);
@@ -109,19 +291,38 @@ class Tgl_instagram
 	}
 
 	/**
-	 * Check Cache
-	 *
-	 * Check for cached data
-	 *
-	 * @access	public
-	 * @param	string
-	 * @param	bool	Allow pulling of stale cache file
-	 * @return	mixed - string if pulling from cache, FALSE if not
+	 * Returns a dynamic name of a cache file, based on parameters that are specified
+	 * @param  array $cache_params a key/value array of parameters to build the cache file name
+	 * @return string
 	 */
-	function _check_cache($url)
-	{	
-		// Check for cache directory
+	function _get_cache_file_name($cache_params){
 		
+		if(! is_array($cache_params) || count($cache_params) < 1)
+		{
+			return FALSE;
+		}
+
+		$filename = "tgl-instagram";
+
+		foreach($cache_params as $key => $value){
+			
+			$filename .= '-'.$key.$value;
+				
+		}
+
+		return $filename;
+		
+	}
+
+	/**
+	 * Checks to see if a cache with the respective cache_params exists, and returns the data
+	 * @param  array $cache_params key/value array of cache params
+	 * @return string/bollean
+	 */
+	function _check_cache($cache_params)
+	{	
+		
+		// Check for cache directory
 		$dir = APPPATH.'cache/'.$this->cache_name.'/';
 		
 		if ( ! @is_dir($dir))
@@ -130,8 +331,7 @@ class Tgl_instagram
 		}
 		
 		// Check for cache file
-		
-        $file = $dir.md5($url);
+    $file = $dir.md5($this->_get_cache_file_name($cache_params));
 		
 		if ( ! file_exists($file) OR ! ($fp = @fopen($file, 'rb')))
 		{
@@ -146,7 +346,7 @@ class Tgl_instagram
         
 		fclose($fp);
 
-        // Grab the timestamp from the first line
+    // Grab the timestamp from the first line
 
 		$eol = strpos($cache, "\n");
 		
@@ -158,9 +358,10 @@ class Tgl_instagram
 			$this->cache_expired = TRUE;
 		}
 		
-        return $cache;
+    return $cache;
+
 	}
-	
+
 	/**
 	 * Write Cache
 	 *
@@ -170,7 +371,7 @@ class Tgl_instagram
 	 * @param	string
 	 * @return	void
 	 */
-	function _write_cache($data, $url)
+	function _write_cache($data, $cache_params)
 	{
 		// Check for cache directory
 		
@@ -192,7 +393,7 @@ class Tgl_instagram
 		
 		// Write the cached data
 		
-		$file = $dir.md5($url);
+		$file = $dir.md5($this->_get_cache_file_name($cache_params));
 	
 		if ( ! $fp = @fopen($file, 'wb'))
 		{
